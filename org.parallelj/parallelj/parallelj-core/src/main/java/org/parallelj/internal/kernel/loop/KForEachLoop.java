ed50270d0fdad2a95bf347750981fc70a1d80442
Thibaut Robert <thibaut.robert@atosorigin.com>
2011-10-12 17:26:39 +0200

    new implementation of KForEach

--- org.parallelj/parallelj/parallelj-core/src/main/java/org/parallelj/internal/kernel/loop/KForEachLoop.java
+++ org.parallelj/parallelj/parallelj-core/src/main/java/org/parallelj/internal/kernel/loop/KForEachLoop.java
@@ -24,46 +24,17 @@ package org.parallelj.internal.kernel.loop;
 
 import java.util.Iterator;
 
-import org.parallelj.internal.kernel.KCondition;
-import org.parallelj.internal.kernel.KElement;
-import org.parallelj.internal.kernel.KCall;
-import org.parallelj.internal.kernel.KInputParameter;
-import org.parallelj.internal.kernel.KProcedure;
-import org.parallelj.internal.kernel.KJoin;
-import org.parallelj.internal.kernel.KProcess;
-import org.parallelj.internal.kernel.KSplit;
+import org.parallelj.internal.kernel.*;
 import org.parallelj.internal.kernel.callback.Iterable;
-import org.parallelj.internal.kernel.join.KAbstractJoin;
+import org.parallelj.internal.kernel.callback.Predicate;
 
 /**
- * Represents a for each loop.
  * 
- * @author Laurent Legrand
+ * 
+ * @author Atos Worldline
  * 
  */
-public class KForEachLoop extends KElement {
-
-	/**
-	 * Flag indicating that the iteration has started for this
-	 * {@link KForEachLoop}.
-	 * 
-	 * <p>
-	 * If the condition {@link Place#contains(short[])} contains no token
-	 * (empty), that means that no action within this {@link KForEachLoop} has
-	 * been started.
-	 * </p>
-	 * <p>
-	 * If the condition contains tokens, the iteration has been started.
-	 * </p>
-	 * 
-	 */
-	private KCondition started;
-
-	/**
-	 * Corresponds to the difference between the number of tokens consumed by
-	 * the first procedure and the number of tokens produced by the last one.
-	 */
-	private KCondition diff;
+public class KForEachLoop extends KWhileLoop {
 
 	/**
 	 * 
@@ -73,7 +44,7 @@ public class KForEachLoop extends KElement {
 	/**
 	 * KParameter that will store the element returned by the iterator.
 	 */
-	private KInputParameter element = new KInputParameter();
+	private final KInputParameter element = new KInputParameter();
 
 	/**
 	 * Create a new for each loop.
@@ -84,78 +55,24 @@ public class KForEachLoop extends KElement {
 	 *            the last procedure of the loop
 	 */
 	public KForEachLoop(final KProcedure first, final KProcedure last) {
-		super(first.getProgram());
-
-		if (first == null || last == null) {
-			throw new IllegalArgumentException("join or split is null");
-		}
-		this.started = new KCondition(program);
-		this.diff = new KCondition(program);
-		first.addInputParameter(this.element);
-		first.setJoin(this.newJoin(first.getJoin()));
-		last.setSplit(this.newSplit(last.getSplit()));
-	}
-
-	/**
-	 * Return a {@link KAbstractJoin} that encapsulated the join passed as
-	 * parameter.
-	 * 
-	 * @param join
-	 * @return
-	 */
-	KJoin newJoin(final KJoin join) {
-		return new KJoin() {
+		super(first, last);
+		first.addInputParameter(element);
+		super.setPredicate(new Predicate() {
 
 			@Override
-			public boolean isEnabled(KProcess process) {
-				return hasNext(process)
-						&& (started.contains(process) || join
-								.isEnabled(process));
+			public boolean verify(KProcess process) {
+				return KForEachLoop.this.hasNext(process);
 			}
-
-			@Override
-			public void join(KCall call) {
-				if (!started.contains(call.getProcess())) {
-					join.join(call);
-					started.produce(call.getProcess());
-				}
-				KForEachLoop.this.element.set(call, KForEachLoop.this.iterable
-						.iterator(call.getProcess()).next());
-				diff.produce(call.getProcess());
-
-			}
-		};
+		});
 	}
 
-	KSplit newSplit(final KSplit split) {
-		return new KSplit() {
-
-			@Override
-			public void split(KCall call) {
-				diff.consume(call.getProcess());
-				if ((diff.isEmpty(call.getProcess()))
-						&& !hasNext(call.getProcess())) {
-					reset(call.getProcess());
-					split.split(call);
-				}
-			}
-		};
-	}
-
-	/**
-	 * Reset the counters (conditions) for a given process
-	 * 
-	 * @param process
-	 *            the process
-	 */
-	public void reset(KProcess process) {
-		this.started.init(process);
-		this.diff.init(process);
-		this.iterable.close(process);
+	@Override
+	protected void iterating(KCall call) {
+		element.set(call, iterable.iterator(call.getProcess()).next());
 	}
 
 	boolean hasNext(KProcess process) {
-		Iterator<?> iterator = this.iterable.iterator(process);
+		Iterator<?> iterator = iterable.iterator(process);
 		return (iterator == null) ? false : iterator.hasNext();
 	}
 
