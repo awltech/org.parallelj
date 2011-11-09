651c68334eb8a73e55b72bbc28786fa1f7bd066e
CWX <christophe.wastiaux@atos.net>
2011-11-09 17:48:38 +0100

    First import of JUnit tests for Configuration

---
+++ org.parallelj/parallelj/parallelj-tools/src/test/java/conf/ConfigurationTest.java
@@ -0,0 +1,62 @@
+/*
+ *     ParallelJ, framework for parallel computing
+ *
+ *     Copyright (C) 2010 Atos Worldline or third-party contributors as
+ *     indicated by the @author tags or express copyright attribution
+ *     statements applied by the authors.
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package conf;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.bind.JAXB;
+
+import org.junit.Test;
+import org.parallelj.tools.conf.Configuration;
+import org.parallelj.tools.conf.Procedures.Procedure;
+
+public class ConfigurationTest {
+
+	@Test
+	public void test() {
+		File file = new File("src/test/java/conf/parallelj.xml");
+		
+		Configuration conf = JAXB.unmarshal(file, Configuration.class);
+		
+		assertNotNull(conf);
+		assertNotNull(conf.getProcedures());
+		assertNotNull(conf.getProcedures().getProcedure());
+		assertEquals(conf.getProcedures().getProcedure().size(), 2);
+		
+		List<Procedure> prs = conf.getProcedures().getProcedure();
+		for (Procedure procedure : prs) {
+			String name = procedure.getName();
+			assertNotNull(name);
+			if (name.equals("pr1")) {
+				assertEquals(procedure.getCapacity().longValue(), 5);
+			}
+			if (name.equals("pr2")) {
+				assertEquals(procedure.getCapacity().longValue(), 10);
+			}
+		}
+	}
+
+}
