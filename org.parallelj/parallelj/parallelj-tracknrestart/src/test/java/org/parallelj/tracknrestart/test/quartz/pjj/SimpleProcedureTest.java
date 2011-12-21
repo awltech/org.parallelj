94050b427eab1f46985c0040e6a19e4627292f99
Pierre Devreux <pierre.devreux@atos.net>
2011-12-21 09:33:51 +0100

    complete test track'n restart

---
+++ org.parallelj/parallelj/parallelj-tracknrestart/src/test/java/org/parallelj/tracknrestart/test/quartz/pjj/SimpleProcedureTest.java
@@ -0,0 +1,15 @@
+package org.parallelj.tracknrestart.test.quartz.pjj;
+
+public class SimpleProcedureTest extends AbstractTest {
+	
+	@Override
+	String getProgramQN() {
+		return "org.parallelj.tracknrestart.test.quartz.pjj.flow.SimpleProcedureProgram";
+	}
+
+
+	int getNumberJob1() {
+		return 30;
+	}
+	
+}
---
+++ org.parallelj/parallelj/parallelj-tracknrestart/src/test/java/org/parallelj/tracknrestart/test/quartz/pjj/WhileLoopTest.java
@@ -0,0 +1,12 @@
+package org.parallelj.tracknrestart.test.quartz.pjj;
+
+
+public class WhileLoopTest extends AbstractTest {
+	
+	@Override
+	String getProgramQN() {
+		return "org.parallelj.tracknrestart.test.quartz.pjj.flow.WhileLoopProgram";
+	}
+
+	
+}
