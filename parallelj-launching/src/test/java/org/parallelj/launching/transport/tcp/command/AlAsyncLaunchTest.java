6b8144dfd94f42c4f52e20d5d59319ddfaa26920
Antoine Neveux <antoine.neveux@gmail.com>
2012-04-10 15:04:45 +0200

    Updated file headers

--- parallelj-launching/src/test/java/org/parallelj/launching/transport/tcp/command/AlAsyncLaunchTest.java
+++ parallelj-launching/src/test/java/org/parallelj/launching/transport/tcp/command/AlAsyncLaunchTest.java
@@ -1,3 +1,24 @@
+/*
+ *     ParallelJ, framework for parallel computing
+ *
+ *     Copyright (C) 2010, 2011, 2012 Atos Worldline or third-party contributors as
+ *     indicated by the @author tags or express copyright attribution
+ *     statements applied by the authors.
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package org.parallelj.launching.transport.tcp.command;
 
 import org.junit.*;
