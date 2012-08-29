8dfc89269e61a1c94c25dbb0ca4bf496c4bca28e
christophe-w <christophe.wastiaux@atos.net>
2012-08-29 15:38:32 +0200

    Add a IMessageHandler to be able to remove LTW messages in the console output LTW messages are in a specific file. Regarding ParallelJ logs, each time a Program is launched, a new log file is created.

---
+++ parallelj-core/src/main/java/org/parallelj/internal/log/LTWMessageHandler.java
@@ -0,0 +1,79 @@
+package org.parallelj.internal.log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Priority;
+import org.apache.log4j.Logger;
+import org.aspectj.bridge.AbortException;
+import org.aspectj.bridge.IMessage;
+import org.aspectj.bridge.IMessage.Kind;
+import org.aspectj.bridge.IMessageHandler;
+
+public class LTWMessageHandler implements IMessageHandler {
+
+	static Logger logger = Logger.getLogger("org.parallelj.weaving");
+
+	Map<Kind, Priority> levelMapping = new HashMap<Kind, Priority>();
+
+	boolean isVerbose = true;
+	boolean isDebug = true;
+	boolean showWeaveInfo = true;
+	boolean showWarn = true;
+
+	public LTWMessageHandler() {
+		levelMapping.put(IMessage.ABORT, Level.TRACE);
+		levelMapping.put(IMessage.DEBUG, Level.DEBUG);
+		levelMapping.put(IMessage.ERROR, Level.ERROR);
+		levelMapping.put(IMessage.FAIL, Level.TRACE);
+		levelMapping.put(IMessage.INFO, Level.INFO);
+		levelMapping.put(IMessage.TASKTAG, Level.TRACE);
+		levelMapping.put(IMessage.WARNING, Level.WARN);
+		levelMapping.put(IMessage.WEAVEINFO, Level.INFO);
+	}
+
+	@Override
+	public boolean handleMessage(IMessage message) throws AbortException {
+		if (isIgnoring(message.getKind())) {
+			return false;
+		} else {
+			logger.log(levelMapping.get(message.getKind()), message.toString());
+		}
+		return true;
+	}
+
+	public boolean isIgnoring(IMessage.Kind kind) {
+		if (kind.equals(IMessage.WEAVEINFO)) {
+			return !showWeaveInfo;
+		}
+		if (kind.isSameOrLessThan(IMessage.INFO)) {
+			return !isVerbose;
+		}
+		if (kind.isSameOrLessThan(IMessage.DEBUG)) {
+			return !isDebug;
+		}
+		return !showWarn;
+	}
+
+	public void dontIgnore(IMessage.Kind kind) {
+		if (kind.equals(IMessage.WEAVEINFO)) {
+			showWeaveInfo = true;
+		} else if (kind.equals(IMessage.DEBUG)) {
+			isVerbose = true;
+		} else if (kind.equals(IMessage.WARNING)) {
+			showWarn = false;
+		}
+	}
+
+	public void ignore(IMessage.Kind kind) {
+		if (kind.equals(IMessage.WEAVEINFO)) {
+			showWeaveInfo = false;
+		} else if (kind.equals(IMessage.DEBUG)) {
+			isVerbose = false;
+		} else if (kind.equals(IMessage.WARNING)) {
+			showWarn = true;
+		}
+	}
+
+}
