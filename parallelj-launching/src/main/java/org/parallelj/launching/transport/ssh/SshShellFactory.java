3f6ab0c5177235b935cffc21f93c3c0cd1f8be92
christophe-w <christophe.wastiaux@atos.net>
2013-03-11 11:59:09 +0100

    Correct SSHD server implementation which  does not handle properly multiple shell clients See https://github.com/awltech/org.parallelj/issues/80

--- parallelj-launching/src/main/java/org/parallelj/launching/transport/ssh/SshShellFactory.java
+++ parallelj-launching/src/main/java/org/parallelj/launching/transport/ssh/SshShellFactory.java
@@ -37,161 +37,162 @@ import org.parallelj.launching.transport.tcp.command.Quit;
 import org.parallelj.launching.transport.tcp.command.TcpCommand;
 import org.parallelj.launching.transport.tcp.command.TcpIpCommands;
 
-public class SshShellFactory implements Factory<Command>, Command, Runnable {
-
-	public static final String ENDLINE = "\n\r";
-
-	private static final String WELCOMEFILE = "/org/parallelj/launching/welcome.txt";
-
-	private static final String CMD_UNKNOWN = "command unknown :";
-
-	private String welcome;
-
-	private InputStream in;
-
-	private OutputStream out;
-
-	private OutputStream err;
-
-	private ExitCallback callback;
-
-	private Thread thread;
-
-	private static final String prompt = "Shell>";
+public class SshShellFactory implements Factory<Command> {
 
 	@Override
 	public Command create() {
-		return this;
+		return new SshShell();
 	}
 
-	@Override
-	public void start(Environment env) throws IOException {
-		thread = new Thread(this, "SshShellFactory");
-		thread.start();
-	}
-
-	@Override
-	public void run() {
-		try {
-			BufferedReader r = new BufferedReader(new InputStreamReader(in));
-			InputStream inputStream = TcpIpHandlerAdapter.class
-					.getResourceAsStream(WELCOMEFILE);
-			InputStreamReader inputStreamReader = new InputStreamReader(
-					inputStream);
-			BufferedReader reader = new BufferedReader(inputStreamReader);
-			StringBuilder sb = new StringBuilder();
-			String line = null;
+	public class SshShell implements Command, Runnable {
+		public static final String ENDLINE = "\n\r";
+	
+		private static final String WELCOMEFILE = "/org/parallelj/launching/welcome.txt";
+	
+		private static final String CMD_UNKNOWN = "command unknown :";
+	
+		private String welcome;
+	
+		private InputStream in;
+	
+		private OutputStream out;
+	
+		private OutputStream err;
+	
+		private ExitCallback callback;
+	
+		private Thread thread;
+	
+		private static final String prompt = "Shell>";
+	
+		@Override
+		public void start(Environment env) throws IOException {
+			thread = new Thread(this, "SshShellFactory");
+			thread.start();
+		}
+	
+		@Override
+		public void run() {
 			try {
-				while ((line = reader.readLine()) != null) {
-					sb.append(line).append(ENDLINE);
+				BufferedReader r = new BufferedReader(new InputStreamReader(in));
+				InputStream inputStream = TcpIpHandlerAdapter.class
+						.getResourceAsStream(WELCOMEFILE);
+				InputStreamReader inputStreamReader = new InputStreamReader(
+						inputStream);
+				BufferedReader reader = new BufferedReader(inputStreamReader);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
+				try {
+					while ((line = reader.readLine()) != null) {
+						sb.append(line).append(ENDLINE);
+					}
+				} catch (IOException e) {
+				} finally {
+					try {
+						reader.close();
+					} catch (IOException e) {
+					}
+					try {
+						inputStream.close();
+					} catch (IOException e) {
+					}
+					try {
+						inputStreamReader.close();
+					} catch (IOException e) {
+					}
+				}
+				this.welcome = sb.toString();
+	
+				out.write(this.welcome.getBytes());
+				out.write(ENDLINE.getBytes());
+	
+				while (true) {
+					out.write(prompt.getBytes());
+					out.flush();
+					while (true) {
+	
+						String str = readLine(r);
+	
+						// Parse the command
+						String cmd = null;
+						String[] args = str.split("[\t ]");
+						if (args.length > 0) {
+							cmd = args[0];
+						}
+	
+						// Try to launch the command
+						TcpCommand command = TcpIpCommands.getCommands().get(cmd);
+						String result = null;
+	
+						// launch the command and get the result
+						if (command != null) {
+							if (command instanceof Quit) {
+								return;
+							} else if (args.length > 1) {
+								result = command.process(null,
+										Arrays.copyOfRange(args, 1, args.length));
+							} else {
+								result = command.process(null, new String[] {});
+							}
+						} else {
+							out.write((CMD_UNKNOWN + cmd).getBytes());
+						}
+						// If the command returned a result, write it for the user
+						if (result != null) {
+							out.write(result.getBytes());
+						}
+						out.write(ENDLINE.getBytes());
+						out.write(ENDLINE.getBytes());
+						out.flush();
+						break;
+					}
 				}
 			} catch (IOException e) {
+				e.printStackTrace();
+				return;
 			} finally {
-				try {
-					reader.close();
-				} catch (IOException e) {
-				}
-				try {
-					inputStream.close();
-				} catch (IOException e) {
-				}
-				try {
-					inputStreamReader.close();
-				} catch (IOException e) {
-				}
+				callback.onExit(0);
 			}
-			this.welcome = sb.toString();
-
-			out.write(this.welcome.getBytes());
-			out.write(ENDLINE.getBytes());
-
-			while (true) {
-				out.write(prompt.getBytes());
-				out.flush();
-				while (true) {
-
-					String str = readLine(r);
-
-					// Parse the command
-					String cmd = null;
-					String[] args = str.split("[\t ]");
-					if (args.length > 0) {
-						cmd = args[0];
-					}
-
-					// Try to launch the command
-					TcpCommand command = TcpIpCommands.getCommands().get(cmd);
-					String result = null;
-
-					// launch the command and get the result
-					if (command != null) {
-						if (command instanceof Quit) {
-							return;
-						} else if (args.length > 1) {
-							result = command.process(null,
-									Arrays.copyOfRange(args, 1, args.length));
-						} else {
-							result = command.process(null, new String[] {});
-						}
-					} else {
-						out.write((CMD_UNKNOWN + cmd).getBytes());
-					}
-					// If the command returned a result, write it for the user
-					if (result != null) {
-						out.write(result.getBytes());
-					}
-					out.write(ENDLINE.getBytes());
-					out.write(ENDLINE.getBytes());
-					out.flush();
-					break;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		} finally {
-			callback.onExit(0);
+	
 		}
-
-	}
-
-	public void setInputStream(InputStream in) {
-		this.in = in;
-	}
-
-	public void setOutputStream(OutputStream out) {
-		this.out = out;
-	}
-
-	public void setErrorStream(OutputStream err) {
-		this.err = err;
-	}
-
-	public void setExitCallback(ExitCallback callback) {
-		this.callback = callback;
-	}
-
-	public void destroy() {
-		thread.interrupt();
-	}
-
-	private String readLine(BufferedReader r) throws IOException {
-		StringBuffer str = new StringBuffer();
-		int ch = r.read();
-		this.out.write(ch);
-		this.out.flush();
-		while ((char) ch != '\n' && ((char) ch != '\r')) {
-			str.append((char) ch);
-			ch = r.read();
+	
+		public void setInputStream(InputStream in) {
+			this.in = in;
+		}
+	
+		public void setOutputStream(OutputStream out) {
+			this.out = out;
+		}
+	
+		public void setErrorStream(OutputStream err) {
+			this.err = err;
+		}
+	
+		public void setExitCallback(ExitCallback callback) {
+			this.callback = callback;
+		}
+	
+		public void destroy() {
+			thread.interrupt();
+		}
+	
+		private String readLine(BufferedReader r) throws IOException {
+			StringBuffer str = new StringBuffer();
+			int ch = r.read();
 			this.out.write(ch);
 			this.out.flush();
+			while ((char) ch != '\n' && ((char) ch != '\r')) {
+				str.append((char) ch);
+				ch = r.read();
+				this.out.write(ch);
+				this.out.flush();
+			}
+			for (int i = 0; i < ENDLINE.length(); i++) {
+				this.out.write(ENDLINE.charAt(i));
+			}
+			this.out.flush();
+	
+			return str.toString();
 		}
-		for (int i = 0; i < ENDLINE.length(); i++) {
-			this.out.write(ENDLINE.charAt(i));
-		}
-		this.out.flush();
-
-		return str.toString();
 	}
-
 }
