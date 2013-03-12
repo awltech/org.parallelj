e5f740840bc2a834c475e7699aa66334caf3f2ae
christophe-w <christophe.wastiaux@atos.net>
2013-03-12 14:55:53 +0100

    Change internal Launcher to use Launching

--- parallelj-launching/src/main/java/org/parallelj/launching/internal/DefaultLauncher.java
+++ parallelj-launching/src/main/java/org/parallelj/launching/internal/DefaultLauncher.java
@@ -1,24 +1,13 @@
 package org.parallelj.launching.internal;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Set;
-
-import org.parallelj.Programs;
 import org.parallelj.internal.reflect.ProcessHelperImpl;
-import org.parallelj.internal.reflect.ProgramAdapter;
-import org.parallelj.launching.In;
 import org.parallelj.launching.errors.ProceduresOnError;
 import org.parallelj.launching.errors.ProceduresOnErrorManagement;
-import org.parallelj.launching.parser.Parser;
-import org.parallelj.launching.quartz.ProgramJobsAdapter;
+import org.parallelj.launching.quartz.Launch;
+import org.parallelj.launching.quartz.LaunchException;
+import org.parallelj.launching.quartz.Launcher;
+import org.parallelj.launching.quartz.QuartzUtils;
+import org.quartz.JobDataMap;
 
 public class DefaultLauncher {
 
@@ -32,40 +21,6 @@ public class DefaultLauncher {
 			int valIndex = arg.indexOf('=');
 			this.argumentName = arg.substring(0, valIndex);
 			this.stringValue = arg.substring(valIndex + 1);
-		}
-
-		public Object getValue(Class<? extends Parser> parser) {
-			Parser prs = null;
-			try {
-				prs = parser.newInstance();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-			return (prs != null ? prs.parse(this.stringValue) : null);
-		}
-
-		public Method getSetMethod(PropertyDescriptor[] propDescs) {
-			for (PropertyDescriptor propertyDescriptor : propDescs) {
-				if (propertyDescriptor.getName().equals(this.argumentName)) {
-					return propertyDescriptor.getWriteMethod();
-				}
-			}
-			return null;
-		}
-
-		private void invokeSetMethod(Object programInstance,
-				Annotation annotation, PropertyDescriptor[] propDescs)
-				throws IllegalArgumentException, IllegalAccessException,
-				InvocationTargetException {
-			Object value = getValue(((In) annotation).parser());
-
-			// Get the set Method for the parameter
-			Method setMethod = getSetMethod(propDescs);
-			if (setMethod != null) {
-				setMethod.invoke(programInstance, value);
-			}
 		}
 	}
 
@@ -87,49 +42,47 @@ public class DefaultLauncher {
 			e.printStackTrace();
 		}
 		
+		ArgEntry[] arguments = new ArgEntry[args.length - 1];
 		try {
-			ArgEntry[] arguments = new ArgEntry[args.length - 1];
 			for (int cpt = 1; cpt < args.length; cpt++) {
 				arguments[cpt - 1] = new ArgEntry(args[cpt]);
 			}
-
-			BeanInfo bi = Introspector.getBeanInfo(programClass);
-			PropertyDescriptor[] propDescs = bi.getPropertyDescriptors();
-
-			for (ArgEntry argEntry : arguments) {
-				Field field = programClass
-						.getDeclaredField(argEntry.argumentName);
-				for (Annotation annotation : field.getAnnotations()) {
-					if (annotation.annotationType().equals(In.class)) {
-						// This is an @In parameter for the Program...
-
-						argEntry.invokeSetMethod(programInstance, annotation,
-								propDescs);
-					}
-				}
-			}
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
 		} catch (ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		} catch (SecurityException e) {
 			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		} catch (IntrospectionException e) {
 			e.printStackTrace();
 		}
 
 		if (programInstance != null) {
 			// Run the Program
-			ProcessHelperImpl<?> processhelper = (ProcessHelperImpl<?>)Programs.as(programInstance).execute().join();
-			ProceduresOnError procOnError = ProceduresOnErrorManagement.getProceduresInErrors(processhelper.getProcess());
-			if (procOnError != null && procOnError. getNumberOfProceduresInError()>0) {
-				System.err.println("Program terminated with errors: "+procOnError);
+			Launcher launcher = null;
+			Launch launch =  null;
+			try {
+				launcher = Launcher.getLauncher();
+				launch = launcher.newLaunch(programClass);
+				for (ArgEntry entry : arguments) {
+					launch.addParameter(entry.argumentName, entry.stringValue);
+				}
+				launch.synchLaunch();
+				
+				// Get the result of the launch and print Program status
+				JobDataMap jobDataMap = launch.getLaunchResult();
+				System.out.println("Program status ["+jobDataMap.get(QuartzUtils.RETURN_CODE)+"] Return code ["+jobDataMap.get(QuartzUtils.USER_RETURN_CODE)+"]");
+				
+				// Get and sho Procedures on error if exists.
+				ProcessHelperImpl<?> processhelper = (ProcessHelperImpl<?>)jobDataMap.get(QuartzUtils.CONTEXT);
+				ProceduresOnError procOnError = ProceduresOnErrorManagement.getProceduresInErrors(processhelper.getProcess());
+				if (procOnError != null && procOnError.getNumberOfProceduresInError()>0) {
+					System.err.println("Program terminated with errors: "+procOnError);
+				}
+			} catch (LaunchException e) {
+				e.printStackTrace();
+			} finally {
+				if (launcher != null) {
+					launcher.complete();
+				}
 			}
 		}
 
