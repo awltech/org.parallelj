6d540d409c363248af8c200aa8d612a38bbc7dc3
Christophe Wastiaux <christophe.wastiaux@atos.net>
2014-09-02 13:18:16 +0200

    Minimize threads numbers in transactional context. See https://github.com/awltech/org.parallelj/issues/99

--- parallelj-launching/src/main/java/org/parallelj/launching/executors/InstanceExecutorService.java
+++ parallelj-launching/src/main/java/org/parallelj/launching/executors/InstanceExecutorService.java
@@ -1,9 +1,13 @@
 package org.parallelj.launching.executors;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.parallelj.internal.conf.pojos.CExecutor;
 import org.parallelj.internal.conf.pojos.ExecutorType;
@@ -12,7 +16,11 @@ import org.parallelj.internal.kernel.KProcess;
 public class InstanceExecutorService extends AbstractExecutorServiceType {
 
 	private Map<KProcess, ExecutorService> executors = new ConcurrentHashMap<>();
-	private Map<String, CExecutor> nonUniqueExecutors = new HashMap<>();
+	private Map<String, CExecutor> cExecutors = new HashMap<>();
+	
+	private List<ExecutorService> instanceExecutorServiceList = new ArrayList<>();   
+
+	private Lock lock = new ReentrantLock();
 
 	@Override
 	public ExecutorService get(KProcess process) {
@@ -21,10 +29,21 @@ public class InstanceExecutorService extends AbstractExecutorServiceType {
 		if (service == null) {
 			// Check if the corresponding process is defined as instance in configuration file
 			// yes => instanciate it...
-			CExecutor cExec = this.nonUniqueExecutors.get(process.getContext().getClass().getCanonicalName());
+			CExecutor cExec = this.cExecutors.get(process.getContext().getClass().getCanonicalName());
 			if (cExec!=null) {
-				service = instanciateExecutor(cExec);
-				this.executors.put(process, service);
+				// look in instanceExecutorServiceList (cache) if one is available
+				try {
+					lock.lock();
+					if (this.instanceExecutorServiceList.size()>0) {
+						service = this.instanceExecutorServiceList.remove(0);
+						this.executors.put(process, service);
+					} else {
+						service = instanciateExecutor(cExec);
+						this.executors.put(process, service);
+					}
+				} finally {
+					lock.unlock();
+				}
 			}
 			if(service == null && process.getParentProcess()!=null) {
 				service = get(process.getParentProcess());
@@ -36,24 +55,43 @@ public class InstanceExecutorService extends AbstractExecutorServiceType {
 	@Override
 	public void complete(KProcess process) {
 		ExecutorService service = this.executors.remove(process);
+		// Put it in instanceExecutorServiceList (cache)
 		if(service != null && !service.isShutdown()) {
-			service.shutdown();
+			try {
+				lock.lock();
+				this.instanceExecutorServiceList.add(service);
+			} finally {
+				lock.unlock();
+			}
 		}
 	}
 
 	@Override
 	public void clean() {
 		for (KProcess knownProcess : this.executors.keySet()) {
-			complete(knownProcess);
+			ExecutorService service = this.executors.remove(knownProcess);
+			if(service != null && !service.isShutdown()) {
+				service.shutdown();
+			}
 		}
-		this.nonUniqueExecutors.clear();
+		try {
+			lock.lock();
+			for (ExecutorService service: this.instanceExecutorServiceList) {
+				if(service != null && !service.isShutdown()) {
+					service.shutdown();
+				}
+			}
+		} finally {
+			lock.unlock();
+		}
+		this.cExecutors.clear();
 	}
 
 	@Override
 	public void add(CExecutor cExecutor) {
 		if (cExecutor.getServiceType()!= null && cExecutor.getType()==ExecutorType.INSTANCE) {
 			for (String name : cExecutor.getProgramName().split(";")) {
-				this.nonUniqueExecutors.put(name, cExecutor);
+				this.cExecutors.put(name, cExecutor);
 			}
 		}
 	}
